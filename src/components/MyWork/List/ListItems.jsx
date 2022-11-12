30265dc8079b2496a86cae773a8a35ad32da41b1
TaiwoJazz <babsman4all@gmail.com>
2022-11-12 18:36:02 +0100

    build for production

--- src/components/MyWork/List/ListItems.jsx
+++ src/components/MyWork/List/ListItems.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { projectList } from '../Helper/Projects';
+import {projectList} from '../Helper/Projects';
 import Card from './Card';
 
-const ListItems = ({ id }) => {
+const ListItems = ({id}) => {
   return (
     <ul className="flex flex-wrap items-start py-8 lg:pl-16 overflow-x-hidden overflow-y-clip">
       {projectList.map((card, index) => (
